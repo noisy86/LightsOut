444dbd3add62a18010d3a4fa3b4fc99998d42997
noisy86 <platon12345546@gmail.com>
2021-02-07 20:20:29 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LightService } from './light.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lightsOut';
+
+
+  boolArr = new Array(5);
+
+  createarr() {
+    for (let i = 0; i < 5; i++) {
+      this.boolArr[i] = new Array(5);
+    }
+    for (let i = 0; i < this.boolArr.length; i++) {
+      for (let j = 0; j < this.boolArr[i].length; j++) {
+        let randomBoolean = Math.random() >= 0.5;
+        this.boolArr[i][j] = randomBoolean;
+      }
+    }
+    console.log(this.boolArr);
+    return this.boolArr;
+  };
+
+click(cell, lineIndex, cellIndex){
+  this.boolArr[lineIndex][cellIndex] = !this.boolArr[lineIndex][cellIndex];
+  if(lineIndex-1 >= 0 ){
+    this.boolArr[lineIndex-1][cellIndex] = !this.boolArr[lineIndex-1][cellIndex];
+  }
+   if(cellIndex-1 >=0 ){
+    this.boolArr[lineIndex][cellIndex-1] = !this.boolArr[lineIndex][cellIndex-1];
+
+  }
+   if(cellIndex+1 < this.boolArr[lineIndex].length){
+    this.boolArr[lineIndex][cellIndex+1] = !this.boolArr[lineIndex][cellIndex+1];
+
+  }
+   if(lineIndex+1 < this.boolArr.length){
+    this.boolArr[lineIndex+1][cellIndex] = !this.boolArr[lineIndex+1][cellIndex];
+  }
+
+  let counter = 0;
+  for (let row = 0; row < this.boolArr.length; row++) {
+    for (let column = 0; column < this.boolArr[row].length; column++) {
+      if(this.boolArr[row][column] == false){
+        counter++;
+      }
+
+    }
+
+  }
+  if(counter == 25){
+    alert("You win");
+  }
+}
+
 }
